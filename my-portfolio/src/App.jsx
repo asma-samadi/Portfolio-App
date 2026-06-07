eec0cd562cef799ad4a1890d967773249c1ccfda
Asma Samadi <asmasamadi001@gmail.com>
2026-06-07 23:20:33 +0430

    Initial commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div>
+      <About />
+      <Contact />
+      <Projects />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
