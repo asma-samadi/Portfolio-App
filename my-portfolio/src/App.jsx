98a81cca39c813fa7ce2dd41b409bc652d73a890
Asma Samadi <asmasamadi001@gmail.com>
2026-06-11 01:00:03 +0430

    Add background effect

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -10,13 +10,27 @@ import { useState } from "react";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
 
   return (
-    <div className={darkMode ? "dark" : "light"}>
+    <div
+      className={darkMode ? "dark" : "light"}
+      onMouseMove={handleMouseMove}
+      style={{
+        "--x": `${mousePosition.x}px`,
+        "--y": `${mousePosition.y}px`,
+      }}
+    >
       <button className="theme-btn" onClick={() => setDarkMode(!darkMode)}>
         {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
       </button>
-
       <div className="layout">
         <div className="left">
           <Hero />
